6d9a845a5bde77b3150bba88dd7cb2da54ae9b6e
Braulio Garcia <brauliogrc95@gmail.com>
2025-08-24 15:15:57 -0600

    Add application routing

--- View/AspiriaTechnicalAssessmentView/src/app/app.routes.ts
+++ View/AspiriaTechnicalAssessmentView/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ToysComponent } from './content/toys-component/toys.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'toys', component: ToysComponent
+    },
+    {
+        path: '**', component: ToysComponent
+    }
+];
---
+++ View/AspiriaTechnicalAssessmentView/src/app/content/toys-component/toys.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ata-toys-component',
+  imports: [],
+  templateUrl: './toys.component.html',
+  styleUrl: './toys.component.scss'
+})
+export class ToysComponent {
+
+}
